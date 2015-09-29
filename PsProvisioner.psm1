aef9dbb910bb69fd7faae2996bb26578d13262af
jebaile7964 <bailey.jonathan@hotmail.com>
2015-09-29 16:57:36 -0500

    syntax changes

--- PsProvisioner.psm1
+++ PsProvisioner.psm1
@@ -1,4 +1,80 @@
-﻿function Initialize-ProvisionerTypes{
+﻿Function Invoke-PsProvisioner{
+    [CmdletBinding(DefaultParameterSetName='GetPrograms')]
+    Param ( [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetname='GetPrograms' )]
+            [switch]$GetPrograms,
+            [Parameter( Position=1,
+                        Mandatory=$false,
+                        ParameterSetName='GetPrograms' )]
+            [switch]$ShowVersion,
+            [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetName='ProvisionPrograms' )]
+            [string]$ProvisionPrograms,
+            [Parameter( Position=1,
+                        Mandatory=$false,
+                        ParameterSetName='Provisionprograms' )]
+            [switch]$provisionProgramsVersion,
+            [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetName='NewDependency' )]
+            [switch]$NewDependency,
+            [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetName='NewDependency' )]
+            [string]$DisplayName,
+            [Parameter( Position=1,
+                        Mandatory=$true,
+                        ParameterSetName='NewDependency' )]
+            [string]$DisplayVersion,
+            [Parameter( Position=2,
+                        Mandatory=$true,
+                        ParameterSetName='NewDependency' )]
+            [string]$Source,
+            [Parameter( Position=3,
+                        Mandatory=$true,
+                        ParameterSetName='NewDependency' )]
+            [string]$ArgumentList,
+            [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetName='CreateManifest' )]
+            [switch]$CreateManifest,
+            [Parameter( Position=1,
+                       Mandatory=$false,
+                       ParameterSetName='CreateManifest' )]
+            [string]$Path,
+            [Parameter( Position=0,
+                        Mandatory=$true,
+                        ParameterSetName='ApplyProvisions' )]
+            [switch]$ApplyProvisions )
+    BEGIN{}
+    PROCESS{
+        switch($PSCmdlet.ParameterSetName){
+            'GetPrograms' { if ( $ShowVersion -eq $false ){ $Output = Get-InstalledProgramsInfo | Select-Object -Property DisplayName }
+                        else{ $Output = Get-InstalledProgramsInfo | Select-Object -Property DisplayName,DisplayVersion }
+                        Write-Output $Output }
+            'CreateConfig' { if ((Test-Path $Path) -eq $true){
+                                Set-Location $Path
+                                New-PsProvisionFile
+                             }
+                             else{
+                                New-PsProvisionFile
+                             } 
+                           }
+            'NewDependency' { New-DependencyRequirement -DisplayName $DisplayName -DisplayVersion $DisplayVersion -Source $Source -argumentlist $ArgumentList }
+            'ProvisionPrograms' { $xmlPath = Join-Path (Get-Location).Path -ChildPath PsProvisioner\Provisions.xml
+                                if ((Test-Path $xmlPath) -eq $false ){
+                                    Export-ProvisionFile
+                                }
+                                Get-DependencyRequirement }
+            'ApplyProvisions' { Install-Dependencies }
+        }
+    }
+    END{}
+}
+
+function Initialize-ProvisionerTypes{
     BEGIN{
         $Source = @"
 using System;
@@ -120,82 +196,6 @@ Total Number of Applications Installed: $z
 "@
         Write-Output $Source
     }
-}
-
-Function Invoke-PsProvisioner{
-    [CmdletBinding(DefaultParameterSetName='GetList')]
-    Param ( [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetname='GetList' )]
-            [switch]$ListAll,
-            [Parameter( Position=1,
-                        Mandatory=$false,
-                        ParameterSetName='GetList' )]
-            [switch]$ShowVersion,
-            [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetName='ProvisionConfig' )]
-            [string]$DesiredProgramDisplayName,
-            [Parameter( Position=1,
-                        Mandatory=$false,
-                        ParameterSetName='ProvisionConfig' )]
-            [switch]$DesiredProgramDisplayVersion,
-            [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetName='NewDependency' )]
-            [switch]$NewDependency,
-            [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetName='NewDependency' )]
-            [string]$DisplayName,
-            [Parameter( Position=1,
-                        Mandatory=$true,
-                        ParameterSetName='NewDependency' )]
-            [string]$DisplayVersion,
-            [Parameter( Position=2,
-                        Mandatory=$true,
-                        ParameterSetName='NewDependency' )]
-            [string]$Source,
-            [Parameter( Position=3,
-                        Mandatory=$true,
-                        ParameterSetName='NewDependency' )]
-            [string]$ArgumentList,
-            [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetName='CreateConfig' )]
-            [switch]$CreateConfigurationFile,
-            [Parameter( Position=1,
-                       Mandatory=$false,
-                       ParameterSetName='CreateConfig' )]
-            [string]$Path,
-            [Parameter( Position=0,
-                        Mandatory=$true,
-                        ParameterSetName='ApplyConfiguration' )]
-            [switch]$ApplyConfiguration )
-    BEGIN{}
-    PROCESS{
-        switch($PSCmdlet.ParameterSetName){
-            'GetList' { if ( $ShowVersion -eq $false ){ $Output = Get-InstalledProgramsInfo | Select-Object -Property DisplayName }
-                        else{ $Output = Get-InstalledProgramsInfo | Select-Object -Property DisplayName,DisplayVersion }
-                        Write-Output $Output }
-            'CreateConfig' { if ((Test-Path $Path) -eq $true){
-                                Set-Location $Path
-                                New-PsProvisionFile
-                             }
-                             else{
-                                New-PsProvisionFile
-                             } 
-                           }
-            'NewDependency' { New-DependencyRequirement -DisplayName $DisplayName -DisplayVersion $DisplayVersion -Source $Source -argumentlist $ArgumentList }
-            'ProvisionConfig' { $xmlPath = Join-Path (Get-Location).Path -ChildPath PsProvisioner\Provisions.xml
-                                if ((Test-Path $xmlPath) -eq $false ){
-                                    Export-ProvisionFile
-                                }
-                                Get-DependencyRequirement }
-            'ApplyConfiguration' { Install-Dependencies }
-        }
-    }
-    END{}
 }
 
 function New-PsProvisionFile{
