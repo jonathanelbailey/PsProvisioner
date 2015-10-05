6b7830bb90308b97cf9e4489b45e090a466301d0
Jonathan Bailey <bailey.jonathan@hotmail.com>
2015-10-05 00:54:45 -0500

    created module template

--- PsProvisioner.psm1
+++ PsProvisioner.psm1
@@ -1,7 +1,5 @@
-﻿$functions = gci functions
-
-foreach ($f in $functions){
-    
-}
+﻿Resolve-Path $PSScriptRoot\*.ps1 |
+? { -not ($_.ProviderPath.Contains(".Tests.")) } |
+% { . $_.ProviderPath }
 
 Export-ModuleMember -Function Invoke-PsProvisioner
